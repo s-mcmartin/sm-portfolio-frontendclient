923dc369d5e9373b17adc7734c08499b99def056
s-mcmartin <shannoncmcguire@outlook.com>
2023-11-10 11:21:13 -0500

    replace broweser router with hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./index.css";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 import App from "./App";
 import { Provider } from "react-redux";
@@ -15,11 +15,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
