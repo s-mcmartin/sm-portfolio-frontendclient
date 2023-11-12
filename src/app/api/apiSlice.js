d7c6669f18a839578f99a49247e680cdfb828fb9
s-mcmartin <shannoncmcguire@outlook.com>
2023-11-12 12:31:50 -0500

    add: baseUrl

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://sm-portfolio-frontendclient.onrender.com",
+  baseUrl: "https://sm-portfolio-api.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
